98024a2f5abe92af19e7c00cb86a7cf9832ef21a
BiancoSofia24 <b.sofia2124@gmail.com>
2021-05-26 17:20:38 -0300

    initial commit, form basic auth with spring security

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Boot Security
+
+Focus on **Spring Boot Security**
+
+## Dependencies
+- Spring Boot Web
+- Spring Boot Security
---
+++ src/main/java/com/example/springbootsecurity/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.example.springbootsecurity.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.springbootsecurity.models.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+        new Student(1L, "Sofia Bianco"),
+        new Student(2L, "Leia Skywalker"),
+        new Student(3L, "Franz Pups")
+    );
+    
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Long id) {
+        return STUDENTS.stream()
+            .filter((s) -> id.equals(s.getId()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("Student " + id + " doesn't exist"));
+    }
+}
